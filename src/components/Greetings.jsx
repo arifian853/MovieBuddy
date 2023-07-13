1475641ff1e3ec75e9745d26389c97341e46f238
Arifian <arifiansaputra43@gmail.com>
2023-07-13 11:19:50 +0700

    Fix responsive

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -4,6 +4,7 @@ export const Greetings = () => {
     <div className="greetings">
         <h1 className="text-3xl font-black">Welcome!</h1>
         <p className="font-black">Search your favourite movies here!</p>
+        <hr />
     </div>
   )
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,7 +4,7 @@ import { PopularMovieCard } from "./PopularMovieCard"
 
 export const MovieList = ({ movies }) => {
     return (
-        <div className="popular flex justify-start align-middle p-12 flex-col gap-5">
+        <div className="popular">
             <h1 className="font-black text-2xl">Popular Movies</h1>
             <div className="container-popular">
                 <PopularMovieCard movies={movies} />
